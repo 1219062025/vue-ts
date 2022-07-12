df05bfa4b6720e41f695d3da66eacf9d015b0ea2
路漫长岁月 <1219062025@qq.com>
2022-07-12 11:46:08 +0800

    test

--- src/views/home.vue
+++ src/views/home.vue
@@ -21,6 +21,7 @@
   // "*.{vue,css,scss}": ["stylelint '**/*.{css,scss}' --fix"],
 
   service.get<Account[]>('/getAccountList').then((res) => {
+    console.log(11);
     console.log(res.data);
   });
 </script>
